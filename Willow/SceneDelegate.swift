bdd2059d938946d45b38412e675f7522aea6d13f
Nick Peachey <nick.peachey@gmail.com>
2019-12-28 06:19:34 +0000

    hhk

--- Willow/SceneDelegate.swift
+++ Willow/SceneDelegate.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow?gjgj
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
